84a78d7738edd3dd1ae40ea76a580addf6213261
krolmiki2011 <mikolajziolkowski504@gmail.com>
2025-07-13 14:27:11 +0200

    Init

---
+++ build-image.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+
+set -e
+set -x
+
+if [ $EUID -ne 0 ]; then
+    echo "$(basename $0) must be run as root"
+    exit 1
+fi
+
+BUILD_USER=${BUILD_USER:-}
+OUTPUT_DIR=${OUTPUT_DIR:-}
+
+source config
+
+if [ -z "${SYSTEM_NAME}" ]; then
+    echo "SYSTEM_NAME must be specified"
+    exit
+fi
+
+if [ -z "${VERSION}" ]; then
+    echo "VERSION must be specified"
+    exit
+fi
+
+DISPLAY_VERSION=${VERSION}
+LSB_VERSION=${VERSION}
+VERSION_NUMBER=${VERSION}
+
+if [ -n "$1" ]; then
+	DISPLAY_VERSION="${VERSION} (${1})"
+	VERSION="${VERSION}_${1}"
+	LSB_VERSION="${LSB_VERSION}　(${1})"
+	BUILD_ID="${1}"
+fi
+
+MOUNT_PATH=/tmp/${SYSTEM_NAME}-build
+BUILD_PATH=${MOUNT_PATH}/subvolume
+SNAP_PATH=${MOUNT_PATH}/${SYSTEM_NAME}-${VERSION}
+BUILD_IMG=/output/${SYSTEM_NAME}-build.img
+
+mkdir -p ${MOUNT_PATH}
+
+fallocate -l ${SIZE} ${BUILD_IMG}
+mkfs.btrfs -f ${BUILD_IMG}
+mount -t btrfs -o loop,compress-force=zstd:15 ${BUILD_IMG} ${MOUNT_PATH}
+btrfs subvolume create ${BUILD_PATH}
+
+cp /etc/makepkg.conf fs/etc/makepkg.conf
+
+pacstrap -K -C fs/etc/pacman.conf ${BUILD_PATH}
+
+mkdir -p fs/etc/pacman.d
+cp /etc/pacman.d/mirrorlist fs/etc/pacman.d/mirrorlist
+
+cp -R config fs/. ${BUILD_PATH}/
+
+# TODO - Other packages (AUR, Local)
+
+# Chroot system
+mount --bind ${BUILD_PATH} ${BUILD_PATH}
+arch-chroot ${BUILD_PATH} /bin/bash -s < ./configure-image.sh
+
+btrfs filesystem defragment -r ${BUILD_PATH}
+
+cp -R fs/. ${BUILD_PATH}/
+echo "${SYSTEM_NAME}-${VERSION}" > ${BUILD_PATH}/build_info
+echo "" >> ${BUILD_PATH}/build_info
+cat ${BUILD_PATH}/config >> ${BUILD_PATH}/build_info
+rm ${BUILD_PATH}/config
+
+if [ -z "${ARCHIVE_DATE}" ]; then
+	export TODAY_DATE=$(date +%Y/%m/%d)
+	echo "Server=https://archive.archlinux.org/repos/${TODAY_DATE}/\$repo/os/\$arch" > \
+	${BUILD_PATH}/etc/pacman.d/mirrorlist
+fi
+
+btrfs subvolume snapshot -r ${BUILD_PATH} ${SNAP_PATH}
+btrfs send -f ${SYSTEM_NAME}-${VERSION}.img ${SNAP_PATH}
+
+cp ${BUILD_PATH}/build_info build_info.txt
+
+#Clean
+umount -l ${BUILD_PATH}
+umount -l ${MOUNT_PATH}
+rm -rf ${MOUNT_PATH}
+rm -rf ${BUILD_IMG}
+
+IMG_FILENAME="${SYSTEM_NAME}-${VERSION}.img.tar.xz"
+if [ -z "${NO_COMPRESS}" ]; then
+	tar -c -I'xz -8 -T4' -f "${IMG_FILENAME}" "${SYSTEM_NAME}-${VERSION}.img"
+	rm "${SYSTEM_NAME}-${VERSION}.img"
+
+	# DZIELENIE na części po 1900 MB
+	split -b 1900M "${IMG_FILENAME}" "${IMG_FILENAME}.part."
+	rm "${IMG_FILENAME}"  # opcjonalnie
+
+	# Tworzenie sumy SHA256 z połączonych części
+	cat ${IMG_FILENAME}.part.* | sha256sum > sha256sum.txt
+	cat sha256sum.txt
+
+	# Przeniesienie do OUTPUT_DIR
+	if [ -n "${OUTPUT_DIR}" ]; then
+		mkdir -p "${OUTPUT_DIR}"
+		mv ${IMG_FILENAME}.part.* "${OUTPUT_DIR}"
+		mv build_info.txt "${OUTPUT_DIR}"
+		mv sha256sum.txt "${OUTPUT_DIR}"
+	fi
+
+	# GitHub Actions output
+	if [ -f "${GITHUB_OUTPUT}" ]; then
+		echo "version=${VERSION}" >> "${GITHUB_OUTPUT}"
+		echo "display_version=${DISPLAY_VERSION}" >> "${GITHUB_OUTPUT}"
+		echo "display_name=${SYSTEM_DESC}" >> "${GITHUB_OUTPUT}"
+		echo "image_filename=${IMG_FILENAME}.part.*" >> "${GITHUB_OUTPUT}"
+	else
+		echo "No github output file set"
+	fi
+else
+	echo "Local build, output IMG directly"
+	if [ -n "${OUTPUT_DIR}" ]; then
+		mkdir -p "${OUTPUT_DIR}"
+		mv ${SYSTEM_NAME}-${VERSION}.img ${OUTPUT_DIR}
+	fi
+fi
---
+++ configure-image.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+
+set -e
+set -x
+
+source /config
+
+pacman-key --populate
+
+locale-gen
+
+# Disable parallel downloads
+sed -i '/ParallelDownloads/s/^/#/g' /etc/pacman.conf
+
+# Cannot check space in chroot
+sed -i '/CheckSpace/s/^/#/g' /etc/pacman.conf
+
+# update package databases
+pacman --noconfirm -Syy
+
+# Disable check and debug for makepkg on the final image
+sed -i '/BUILDENV/s/ check/ !check/g' /etc/makepkg.conf
+sed -i '/OPTIONS/s/ debug/ !debug/g' /etc/makepkg.conf
+
+# install kernel package
+if [ "$KERNEL_PACKAGE_ORIGIN" == "local" ] ; then
+	pacman --noconfirm -U --overwrite '*' \
+	/override_pkgs/${KERNEL_PACKAGE}-*.pkg.tar.zst
+else
+	pacman --noconfirm -S "${KERNEL_PACKAGE}" "${KERNEL_PACKAGE}-headers"
+fi
+
+pacman --noconfirm -Rdd jack2 || true
+
+pacman --noconfirm -S --overwrite '*' --disable-download-timeout ${PACKAGES}
+rm -rf /var/cache/pacman/pkg
+
+# Install the new iptables
+# See https://gitlab.archlinux.org/archlinux/packaging/packages/iptables/-/issues/1
+# Since base package group adds iptables by default
+# pacman will ask for confirmation to replace that package
+# but the default answer is no.
+# doing yes | pacman omitting --noconfirm is a necessity
+yes | pacman -S iptables-nft
+
+# enable services
+systemctl enable ${SERVICES}
+
+# enable user services
+systemctl --global enable ${USER_SERVICES}
+
+# disable root login
+passwd --lock root
+
+# set the default editor, so visudo works
+echo "export EDITOR=nano" >> /etc/bash.bashrc
+
+# enable multicast dns in avahi
+sed -i "/^hosts:/ s/resolve/mdns resolve/" /etc/nsswitch.conf
+
+# configure ssh
+echo "
+AuthorizedKeysFile	.ssh/authorized_keys
+PasswordAuthentication no
+ChallengeResponseAuthentication no
+UsePAM yes
+PrintMotd no # pam does that
+Subsystem	sftp	/usr/lib/ssh/sftp-server
+" > /etc/ssh/sshd_config
+
+echo "
+LABEL=zenkai_root /var       btrfs     defaults,subvolid=256,rw,noatime,nodatacow,nofail                                                                                                                                                                                                                      0   0
+LABEL=zenkai_root /home      btrfs     defaults,subvolid=257,rw,noatime,nodatacow,nofail                                                                                                                                                                                                                      0   0
+LABEL=zenkai_root /zenkai_root btrfs     defaults,subvolid=5,rw,noatime,nodatacow,x-initrd.mount                                                                                                                                                                                                                0   2
+overlay         /etc       overlay   defaults,x-systemd.requires-mounts-for=/zenkai_root,x-systemd.requires-mounts-for=/sysroot/zenkai_root,x-systemd.rw-only,lowerdir=/sysroot/etc,upperdir=/sysroot/zenkai_root/etc,workdir=/sysroot/zenkai_root/.etc,index=off,metacopy=off,comment=etcoverlay,x-initrd.mount    0   0
+" > /etc/fstab
+
+echo "
+LSB_VERSION=1.4
+DISTRIB_ID=${SYSTEM_NAME}
+DISTRIB_RELEASE=\"${LSB_VERSION}\"
+DISTRIB_DESCRIPTION=${SYSTEM_DESC}
+" > /etc/lsb-release
+
+echo 'NAME="${SYSTEM_DESC}"
+VERSION="${DISPLAY_VERSION}"
+VERSION_ID="${VERSION_NUMBER}"
+BUILD_ID="${BUILD_ID}"
+PRETTY_NAME="${SYSTEM_DESC} ${DISPLAY_VERSION}"
+ID=${SYSTEM_NAME}
+ID_LIKE=arch
+ANSI_COLOR="1;33"
+HOME_URL="${WEBSITE}"
+DOCUMENTATION_URL="${DOCUMENTATION_URL}"
+BUG_REPORT_URL="${BUG_REPORT_URL}"' > /usr/lib/os-release
+
+postinstallhook
+
+
+pacman -Q > /config
+
+# preserve installed package database
+mkdir -p /usr/var/lib/pacman
+cp -r /var/lib/pacman/local /usr/var/lib/pacman/
+
+# move kernel image and initrd to a defualt location if "linux" is not used
+if [ ${KERNEL_PACKAGE} != 'linux' ] ; then
+	mv /boot/vmlinuz-${KERNEL_PACKAGE} /boot/vmlinuz-linux
+	mv /boot/initramfs-${KERNEL_PACKAGE}.img /boot/initramfs-linux.img
+	mv /boot/initramfs-${KERNEL_PACKAGE}-fallback.img /boot/initramfs-linux-fallback.img
+fi
+
+# clean up/remove unnecessary files
+rm -rf \
+/home \
+/var \
+
+rm -rf ${FILES_TO_DELETE}
+
+# create necessary directories
+mkdir -p /home
+mkdir -p /var
+mkdir -p /zenkai_root
+mkdir -p /efi
